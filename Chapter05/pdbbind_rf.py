20b4b2eeff421d331722637899845e4c9a4a52a6
Giulia Cantini <giuliacantini3@gmail.com>
2020-08-10 17:48:00 +0200

    Add Chapter 5 comments

--- Chapter05/pdbbind_rf.py
+++ Chapter05/pdbbind_rf.py
@@ -1,7 +1,16 @@
 # Use a random forest to predict the PDBBind dataset.  First load the data.
 
 import deepchem as dc
-pdbbind_tasks, pdbbind_datasets, transformers = dc.molnet.load_pdbbind(featurizer="grid", split="random", subset="core")
+# Perform GridFeaturization automatically during loading
+# In alternative use RdkitGridFeaturizer
+# grid_featurizer = dc.feat.RdkitGridFeaturizer(
+#    voxel_width=2.0,
+#    feature_types=['hbond', 'salt_bridge', 'pi_stack', 'carton_pi', 'ecfp', 'splif'],
+#    sanitize=True,
+#    flatten=True)
+
+pdbbind_tasks, pdbbind_datasets, transformers = dc.molnet.load_pdbbind(
+    featurizer="grid", split="random", subset="core")
 train_dataset, valid_dataset, test_dataset = pdbbind_datasets
 
 # Create and train the model.
@@ -12,7 +21,9 @@ model = dc.models.SklearnModel(sklearn_model, model_dir="pdbbind_rf")
 model.fit(train_dataset)
 
 # Evaluate it.
-
+# Pearson R^2 is in [-1, 1]. 
+# 0 means no correlation between true and predicted labels
+# 1 indicates perfect correlation
 metric = dc.metrics.Metric(dc.metrics.pearson_r2_score)
 train_scores = model.evaluate(train_dataset, [metric], transformers)
 test_scores = model.evaluate(test_dataset, [metric], transformers)
